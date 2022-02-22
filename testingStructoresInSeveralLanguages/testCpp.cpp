c372d5ba82238e4f98489d9b719b0cf07edc429d
dmitriy <dim4ik-super-klass@mail.ru>
2022-02-23 01:10:10 +0800

    add c++ testing

--- testingStructoresInSeveralLanguages/testCpp.cpp
+++ testingStructoresInSeveralLanguages/testCpp.cpp
@@ -7,20 +7,31 @@ using namespace std;
 
 const int n_exp = 8; 
 
+typedef map<int, int> test_map;
+
 int main(){
-	cout << "size of Allocator - " << sizeof(map<int,int>::allocator_type) << endl;
-	cout << "size of Reference - " << sizeof(map<int, int>::reference) << endl;
-	cout << "size of value_type - " << sizeof(map<int,int>::value_type) << endl;
-	cout << "size of mapped+key type - " << sizeof(map<int,int>::mapped_type)
-						+sizeof(map<int,int>::key_type) << endl;
+	/*
+	cout << "size of Allocator - " << sizeof(test_map::allocator_type) << endl;
+	cout << "size of Pointer - " << sizeof(test_map::pointer) << endl;
+	cout << "size of value_type - " << sizeof(test_map::value_type) << endl;
+	*/
 	for(int i = 0; i < n_exp; i++){
 		int n = floor(pow(10, i)+0.5);
-		map<int,int> test_m;
+		auto start = chrono::high_resolution_clock::now();
+		test_map test_m;
+		
 		for(int j = 0; j < n; j++){
 			test_m.insert({j,j});		
 		}
-		cout << test_m.size()*sizeof(map<int,int>::value_type) << endl;
+
+		auto stop = chrono::high_resolution_clock::now();	
+		
+		cout << test_m.size()*sizeof(test_map::value_type)
+		       	+sizeof(test_map::pointer) 
+			+sizeof(test_map::allocator_type)	
+			<< " ";
+		
+		cout << chrono::duration<long long, nano>(stop-start).count() << "\n";
 	}	
-	cout << "FUCK yourself" << endl;
 	return 0;
 }
