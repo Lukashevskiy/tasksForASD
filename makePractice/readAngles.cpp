65d65ac72f243c5febb485893ceeeedfcf965942
dmitriy <dim4ik-super-klass@mail.ru>
2022-03-05 16:21:15 +0800

    fix in read_angles

--- makePractice/readAngles.cpp
+++ makePractice/readAngles.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-double read_angle(double * angles){
-	return 2 * sin(angles[0]) * sin(angels[1]) + cos(angels[2]);  
+double read_angle(double * angels){
+	return 2 * sin(angels[0]) * sin(angels[1]) + cos(angels[2]);  
 }
 
 int main(){
-	double * ang = new double[3];
+	double *ang = new double[3];
 	do{		
 		cin >> ang[0] >> ang[1] >> ang[2];
 		cout << read_angle(ang) << "\n";
-	}while(!cin);
+	}while(cin);
 	return 0;
 }
